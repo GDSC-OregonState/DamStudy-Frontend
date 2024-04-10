c1b32e34cd70370a0053bdd498f7ca6fb9f1ba34
Thomas <nyumat18@gmail.com>
2024-04-09 17:37:08 -0700

    responsive hero + discord link

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,23 +1,37 @@
+import { cn } from "@/lib/utils";
+import { DiscordLogoIcon } from "@radix-ui/react-icons";
 import { useNavigate } from "react-router-dom";
 import { AspectRatio } from "./ui/aspect-ratio";
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 
 const Hero = () => {
   const navigate = useNavigate();
   return (
     <section id="hero">
-      <div className="container flex flex-col md:flex-row mx-auto overflow-x-clip">
-        <div className="space-y-6 md:w-1/2 my-12">
-          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl font-sans text-center md:text-left">
+      <div className="container flex flex-col lg:flex-row mx-auto lg:overflow-x-clip">
+        <div className="space-y-6 lg:w-1/2 my-12">
+          <a
+            href="https://discord.gg/pvfa69B4gb"
+            className={cn(
+              buttonVariants({ variant: "outline", size: "sm" }),
+              "flex items-center justify-start w-min self-center place-self-center mx-auto"
+            )}
+            target="_blank"
+          >
+            This project is a work in progress. Join the GDSC@OSU Discord to
+            contribute!
+            <DiscordLogoIcon className="ml-2 size-4" />
+          </a>
+          <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl font-sans text-center lg:text-left">
             The Ultimate
             <span className="text-orange-600"> Study Room </span>
             Finder For <span className="text-orange-600">OSU</span> Students
           </h1>
-          <p className="text-lg md:text-left text-center text-neutral-600 dark:text-neutral-400 md:w-5/6">
+          <p className="text-lg max-w-xl lg:max-w-md mx-auto lg:mx-2 lg:text-left text-center text-neutral-600 dark:text-neutral-400 lg:w-5/6">
             Discover the best study rooms on campus. Leave reviews, explore what
             each room has to offer, and find the perfect spot to study.
           </p>
-          <div className="flex gap-4 justify-center md:justify-start">
+          <div className="flex gap-4 justify-center lg:justify-start">
             <Button variant="default" onClick={() => navigate("/gallery")}>
               Get started
             </Button>
@@ -26,12 +40,12 @@ const Hero = () => {
             </Button>
           </div>
         </div>
-        <div className="md:w-1/2">
+        <div className="lg:w-1/2 lg:mt-20">
           <AspectRatio ratio={16 / 9}>
             <img
               src="/preview.png"
               alt="App preview"
-              className="object-cover rounded-lg ring-1 ring-neutral-200 dark:ring-neutral-700 p-1 scale-150 translate-x-32"
+              className="object-cover rounded-lg ring-1 ring-neutral-200 dark:ring-neutral-700 p-1 lg:scale-150 lg:translate-x-32"
             />
           </AspectRatio>
         </div>
